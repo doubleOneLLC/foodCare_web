edccbda04ceb6b91435a056b200e576441935bf0
xyzuan <xyzuannihboss@gmail.com>
2023-10-05 16:05:26 +0700

    main: footer: Create Footer components

--- src/app/pages/home/home.page.js
+++ src/app/pages/home/home.page.js
@@ -5,12 +5,14 @@ import React from "react";
 
 import Navbar from "@/app/components/navbar";
 import HeroSection from "./sections/hero.section";
+import Footer from "@/app/components/footer";
 
 function HomePage() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Footer />
     </>
   );
 }
