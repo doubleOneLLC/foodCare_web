47e40850b4e8a56c2a6d95968d471f576b925246
Jody Yuantoro <xyzuannihboss@gmail.com>
2023-10-05 16:54:46 +0700

    main: Remove MaterialUI components

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
-//
-import { Container } from "@mui/material";
-
 // Pages
 import HomePage from "./pages/home/home.page.js";
 
 export default function Home() {
   return (
-    <Container>
+    <>
       <HomePage />
-    </Container>
+    </>
   );
 }
