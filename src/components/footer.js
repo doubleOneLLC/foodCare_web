ebe15f857903b6a037d54439869453a65e8512b3
xyzuan <xyzuannihboss@gmail.com>
2023-10-11 21:46:43 +0700

    main: navbar: Adjust mobile padding

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="bg-white">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6">
+      <div className="mx-auto w-full max-w-screen-xl px-[32px] py-6">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0 max-w-[300px]">
             <a className="flex items-center pb-6">
