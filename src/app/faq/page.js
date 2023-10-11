ccbdcbad46ba831b77cb967d23dce94a85dd538c
xyzuan <xyzuannihboss@gmail.com>
2023-10-11 15:44:11 +0700

    main: faq: Responsive FAQ

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -6,12 +6,10 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="container max-w-screen-xl mt-32 mb-8 ">
+      <div className="container max-w-screen-xl mt-32 mb-8 px-8">
         <div className="gap-12 grid md:grid-cols-2">
           <div>
-            <h1 className="font-bold text-3xl px-4 mb-4">
-              Soal sering ditanya
-            </h1>
+            <h1 className="font-bold text-3xl mb-4">Soal sering ditanya</h1>
             <Accordion
               title="Apa itu donasi makanan?"
               summary="Donasi makanan adalah tindakan sukarela di mana individu, kelompok, atau organisasi menyumbangkan makanan kepada mereka yang membutuhkan. Makanan yang didonasikan dapat berasal dari sisa makanan, surplus, atau makanan baru yang disiapkan khusus untuk tujuan donasi."
@@ -28,7 +26,7 @@ export default function Home() {
             />
           </div>
           <div>
-            <h1 className="font-bold text-3xl  mb-4">
+            <h1 className="font-bold text-3xl mb-8">
               Masih belum menemukan jawaban?
             </h1>
             <div className="grid gap-4">
@@ -84,7 +82,7 @@ export default function Home() {
                 >
                   Pesan
                 </label>
-                <input
+                <textarea
                   type="text"
                   id="message"
                   className="block w-full p-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-[20px] focus:ring-primary focus:border-primary dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary dark:focus:border-primary"
