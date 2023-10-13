902c543451e63fdb61f65c9367a88bca3a479c68
xyzuan <xyzuannihboss@gmail.com>
2023-10-13 17:30:08 +0700

    main: donate/money: Implement rpFormatter for nominal val

--- src/app/donate/money/components/input-nominal.js
+++ src/app/donate/money/components/input-nominal.js
@@ -1,33 +1,28 @@
-// components/InputNominal.js
 "use client";
-import React from "react";
+import { formatRupiah } from "@/utils/formatRp.utils";
+import React, { useState } from "react";
 
-const maxLength = 10;
-
-const maxLenghtCheck = (e) => {
-  if (e.target.value.length > maxLength) {
-    e.target.value = e.target.value.slice(0, maxLength);
-  }
-};
 const InputNominal = () => {
+  const [donateVal, setDonateVal] = useState("");
+
+  const handleInputChange = (e) => {
+    const formattedValue = formatRupiah(e.target.value, "Rp. ");
+    setDonateVal(formattedValue);
+  };
+
   return (
     <div className="p-6 rounded-[32px] w-[100%] flex flex-col justify-between bg-gray-50">
       <p className="md:mb-3">Masukkan Nominal</p>
       <div className="inline w-auto">
-        <span
-          id="tpl"
-          className="font-bold text-gray-900 md:text-4xl text-3xl flex"
-        >
-          Rp.
-          <input
-            id="nominal"
-            className="font-bold text-gray-900 md:text-4xl text-3xl  bg-transparent border-transparent p-0 placeholder-gray-300 focus:bg-transparent focus:ring-transparent focus:border-transparent [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-            placeholder="1.000.000"
-            onInput={(e) => maxLenghtCheck(e)}
-            maxLength={maxLength}
-            type="number"
-          ></input>
-        </span>
+        <input
+          type="text"
+          id="nominal"
+          className="font-bold text-gray-900 md:text-4xl text-3xl  bg-transparent border-transparent p-0 placeholder-gray-300 focus:bg-transparent focus:ring-transparent focus:border-transparent [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+          placeholder="Rp"
+          maxLength="16"
+          value={donateVal}
+          onChange={handleInputChange}
+        />
       </div>
     </div>
   );
